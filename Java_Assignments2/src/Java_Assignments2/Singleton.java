15f990235ea99bca472fe6c102d70cfdda8599b3
mfaisher <mfaisher@lin80033520.corp.capgemini.com>
2021-11-14 11:49:52 +0530

    Added Singleton Assignment

---
+++ Java_Assignments2/src/Java_Assignments2/Singleton.java
@@ -0,0 +1,25 @@
+package Java_Assignments2;
+
+import java.io.*;
+import java.util.*;
+
+public class Singleton {
+private static Singleton single=new Singleton();
+	
+	//Create private constructor that restricts the object to create object
+	//outside the class.
+	private Singleton()
+	{
+		
+	}
+	//Allows us to create and access the object we created.
+	public static Singleton getInstance()
+	{
+		return single;
+	}
+	public void getInfo()
+	{
+		System.out.println("In Singleton class...!!!");
+	}
+
+}
---
+++ Java_Assignments2/src/Java_Assignments2/SingletonEx.java
@@ -0,0 +1,10 @@
+package Java_Assignments2;
+
+public class SingletonEx {
+	public static void main(String[] args) 
+	{
+		Singleton ex=Singleton.getInstance();
+		ex.getInfo();
+
+	}
+}
