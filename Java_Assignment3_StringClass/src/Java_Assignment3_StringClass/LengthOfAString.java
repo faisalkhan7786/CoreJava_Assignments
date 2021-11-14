1f656c95048b4543d2338168641c781e5b4fa71e
mfaisher <mfaisher@lin80033520.corp.capgemini.com>
2021-11-14 11:52:19 +0530

    Added StringClass Assignment

---
+++ Java_Assignment3_StringClass/src/Java_Assignment3_StringClass/LengthOfAString.java
@@ -0,0 +1,9 @@
+package Java_Assignment3_StringClass;
+
+public class LengthOfAString {
+	public static void main(String[] args) 
+	{
+		String str ="Hello World";
+		System.out.println(str.length());
+	}
+}
